ef035b2c6c000cd4ac6343d7fdfed16ad39af0af
marcelocarmona <marcelo@marcelocarmona.com>
2017-06-05 02:32:37 -0300

    express-graphql

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express')
+var graphqlHTTP = require('express-graphql')
+var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -12,8 +14,11 @@ var root = {
   hello: () => {return 'Hello world'}
 }
 
-
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then( (response) => {
-  console.log(response)
-})
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
